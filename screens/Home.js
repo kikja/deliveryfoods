59dcf735aae3b0f0f946c95e533f78390f20915e
kikja <chaladha.sakuntra@gmail.com>
2022-05-24 17:00:18 +0700

    add HeaderTabs searchBar Categories
    add HeaderTabs  searchBar Categories

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,25 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import HeaderTabs from '../components/HeaderTabs'
+import { SafeAreaView } from 'react-native'
+import SearchBar from '../components/SearchBar'
+import Categories from '../components/Categories'
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{
+      backgroundColor: "#eee",
+      flex: 1,
+
+    }}>
+      <View style={{
+        backgroundColor: "white",
+        padding: 15
+      }}>
+        <HeaderTabs />
+        <SearchBar />
+        <Categories />
+      </View>
+    </SafeAreaView>
   )
 }
